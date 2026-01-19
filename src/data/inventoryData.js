f9bb07ac757e26d64e382af9754cbbf8b4f4b8ab
Agyemang8172 <godfredagyemang31052005@gmail.com>
2026-01-19 12:52:29 +0000

    First Commit

---
+++ src/data/inventoryData.js
@@ -0,0 +1,27 @@
+export const inventoryData = [
+    { 
+        id:1, 
+        name:'Rice',
+        category:'Grains',
+        quantity: 20,
+         minStock:10,
+         price : $56
+    },
+    { 
+        id:2, 
+        name:'Fanta',
+        category:'soft drinks',
+        quantity: 100,
+         minStock:20,
+         price : $20
+    },
+    { 
+        id:3, 
+        name:'Kingsbite',
+        category:'chocolate',
+        quantity: 50,
+         minStock:10,
+         price : $92
+    }
+   
+]
