0e5fcfb7891bdb807b0108079da68062b0c4843d
Agyemang8172 <godfredagyemang31052005@gmail.com>
2026-04-24 11:57:17 +0000

    Adding middleware and auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authmiddleware = require('../middleware/authMiddleware')
 
 
 
-router.get('/',userController.getAllUsers)
+router.get('/', authmiddleware, userController.getAllUsers)
 
-router.get('/:id',userController.getUserById)
+router.get('/:id', authmiddleware,userController.getUserById)
 
-router.post('/' , userController.createUser)
+router.post('/', userController.createUser)
 
-router.put('/:id', userController.updateUser)
+router.put('/:id',authmiddleware, userController.updateUser)
 
-router.delete('/:id', userController.deleteUser)
+router.delete('/:id',authmiddleware, userController.deleteUser)
 
 module.exports = router
