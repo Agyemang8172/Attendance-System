6704701614a084bc7a7817e514f09829be805434
Agyemang8172 <godfredagyemang31052005@gmail.com>
2026-06-11 08:39:15 +0000

    added font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Fraunces: ['Fraunces', 'serif'],
+        GeneralSans: ['General Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
